d38faea7a16f5aa8917337c87f3197e33edbe6b2
Bikcode <bikcodeh@gmail.com>
2024-02-14 07:07:42 -0500

    create confirm and new password layouts

--- src/pages/confirm-account.page.tsx
+++ src/pages/confirm-account.page.tsx
@@ -1,5 +1,7 @@
 export const ConfirmAccountPage = () => {
   return (
-    <div>ConfirmAccountPage</div>
+    <>
+     <h1 className="text-sky-600 font-black text-6xl capitalize">Confirm your account and manage your <span className="text-slate-700">projects</span></h1>
+    </>
   )
 }
--- src/pages/new-password.page.tsx
+++ src/pages/new-password.page.tsx
@@ -1,5 +1,33 @@
 export const NewPasswordPage = () => {
   return (
-    <div>NewPasswordPage</div>
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">Change your password and manage your <span className="text-slate-700">projects</span></h1>
+      <form className="my-10 bg-white shadow rounded-lg px-10 py-5">
+        <div className="my-5">
+          <label
+            htmlFor="new_password"
+            className="
+              uppercase 
+              text-gray-600 
+              block 
+              text-xl 
+              font-bold"
+          >
+            New Password
+          </label>
+          <input
+            id="new_password"
+            type="password"
+            placeholder="Your new password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <input
+            type="submit"
+            value="Save new password"
+            className="bg-sky-700 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+          />
+      </form>
+    </>
   )
 }
